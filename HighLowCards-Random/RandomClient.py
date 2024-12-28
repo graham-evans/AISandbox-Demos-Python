f1cf3817a5887bda21395f84bcf7801493b95814
Graham Evans <gde@pobox.com>
2024-12-28 14:30:54 +0000

    update to latest proto standard

--- HighLowCards-Random/RandomClient.py
+++ HighLowCards-Random/RandomClient.py
@@ -16,22 +16,21 @@ stream = connection.makefile(mode='rwb')
 while True:
 
     # read play state
-    state = dp.read(stream,HighLowCards.HighLowCardsState)
+    state = dp.read(stream, HighLowCards.HighLowCardsState)
 
     print("state:")
     print(state)
 
-    # are we asked for an action?
-    if state.signal == HighLowCards.Signal.PLAY:
+    # send random response
+    action = HighLowCards.HighLowCardAction()
+    if (random.randint(0, 1) == 0):
+        action.action = HighLowCards.HighLowChoice.LOW
+    else:
+        action.action = HighLowCards.HighLowChoice.HIGH
 
-        print("Sending action")
+    print("Sending action")
+    dp.write(stream, action)
+    stream.flush()
 
-        action = HighLowCards.HighLowCardAction()
-        if (random.randint(0,1)==0):
-            action.action = HighLowCards.HighLowChoice.LOW
-        else:
-            action.action = HighLowCards.HighLowChoice.HIGH
-
-        dp.write(stream,action)
-        stream.flush()
-
+    # read result
+    dp.read(stream,HighLowCards.HighLowCardsResult)
