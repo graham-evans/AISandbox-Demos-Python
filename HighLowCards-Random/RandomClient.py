8ebb243ade3d59dcdbec053c4b91b93e13e47a16
graham-evans <2950775+graham-evans@users.noreply.github.com>
2025-09-05 23:21:32 +0100

    allow CLI arguments to overwrite the host / port

--- HighLowCards-Random/RandomClient.py
+++ HighLowCards-Random/RandomClient.py
@@ -1,14 +1,24 @@
 import random
+import sys
 
 from google.protobuf.internal import decoder
 import HighLowCards_pb2 as HighLowCards
 import delimited_protobuf as dp
 import socket
 
-# connect to the server running on locahost, port 9000
+# connect to the server using localhost and 9000 as defaults
+
+host = "localhost"
+port = 9000
+
+if len(sys.argv) == 2:
+    port = int(sys.argv[1])
+elif len(sys.argv) == 3:
+    host = sys.argv[1]
+    port = int(sys.argv[2])
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("localhost", 9000))
+connection.connect((host, port))
 
 # create input and output streams
 stream = connection.makefile(mode='rwb')
